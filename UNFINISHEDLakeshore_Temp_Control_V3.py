6133c58c9ffb7d1a75c1d2719b9f670ed12a9d31
ChrisCravey1810 <christopheracravey@gmail.com>
2021-12-06 12:18:45 -0500

    I do be changing things
    Must edit files located in document/github

--- UNFINISHEDLakeshore_Temp_Control_V3.py
+++ UNFINISHEDLakeshore_Temp_Control_V3.py
@@ -24,8 +24,7 @@ start_time = dt.datetime.now()
 '''
 my_instrument = Model372(9600, ip_address = '169.254.110.0')
 
-print("oof")
-print("hot diggidy")
+print("I do be changing things")
 #ENTER CUSTOM FILENAME
 #OR 
 #LEAVE EMPTY ('') and a name will be generated based on today's date
